f5ebdc36a84cf2b912597b84dc606580f8e96eb4
Peter Jones <pjones@devalot.com>
2017-01-20 15:22:18 -0700

    Ensure `gnutls-cli' is in PATH for Circe (IRC client)

--- sets/base.nix
+++ sets/base.nix
@@ -7,11 +7,13 @@
     # Emacs, my best friend.  But it's better without GTK. (The Emacs
     # daemon works better without GTK, that is.)
     (emacs.override {withX = true; withGTK2 = false; withGTK3 = false;})
+    gnutls # Needed by various Emacs packages.
 
     # I still have a lot of scripts that require Ruby.
     ruby_2_2
 
     tmux duplicity gnupg inotifyTools libxml2 libxslt
     rsync unison zip unzip tree bc pwgen
+
   ];
 }
