b1fc66f9d69cc5698fc52835718f1c3d6f687738
Peter Jones <pjones@devalot.com>
2017-02-06 10:58:47 -0700

    Continue working on a Nix file to build TWRP for my phone

--- envs/dev/twrp.nix
+++ envs/dev/twrp.nix
@@ -1,13 +1,41 @@
+# This derivation is based on the following guides (and help from #twrp):
+#
+#   http://wiki.lineageos.org/oneplus3_build.html
+#   https://forum.xda-developers.com/showthread.php?t=1943625
+#
+# More information:
+#
+#   https://github.com/omnirom/android_bootable_recovery
+#
+# To do:
+#
+#   * Add roomserviceLines to .repo/local_manifests/roomservice.xml
+#   * Continue fixing wacky shit.
+
 let
-  pkgs     = import <nixpkgs> {};
-  stdenv   = pkgs.stdenv;
-  fetchgit = pkgs.fetchgit;
+  pkgs       = import <nixpkgs> {};
+  stdenv     = pkgs.stdenv;
+  fetchgit   = pkgs.fetchgit;
+
+  repo       = "git://github.com/omnirom/android.git";
+  branch     = "android-7.1";
+
+  # NOTE: Change this to match your Device Code:
+  deviceCode = "oneplus3";
+
+  # NOTE: These are extra `repo' source that you need that contain
+  # binary blobs for your device.  You'll need to track down which
+  # repositories you actually need.
+  #
+  # https://github.com/TheMuppets
+  roomserviceLines = ''
+    <project name="TheMuppets/proprietary_vendor_oneplus" path="vendor/oneplus"/>
+    <project name="TheMuppets/proprietary_vendor_qcom_binaries" path="vendor/qcom/binaries"/>
+  '';
 
 in stdenv.mkDerivation {
   name    = "twrp";
   version = "3.0.4";
-  repo    = "git://github.com/omnirom/android.git";
-  branch  = "android-7.1";
 
   src = fetchgit {
     url    = repo;
@@ -18,17 +46,28 @@ in stdenv.mkDerivation {
   buildInputs = with pkgs; [
     gitRepo # the repo tool
     gnupg   # wanted by the repo tool
+    python  # need for building
   ];
 
   postUnpack = ''
-    [ -n "$out" ] && cd "$out"
+    [ -n "$out" -a -d "$out" ] && cd "$out"
     repo init -u ${repo} -b ${branch}
     repo sync
+
+    # Fix stupid absolute paths all over the place:
+    for f in `find . -type f -name '*.sh' -o -name Makefile -o -name '*.mk'`; do
+      sed -i.orig -e 's|/bin/bash|${pkgs.bash}/bin/bash|g' \
+                  -e 's|/bin/||g' \
+                   "$f"
+    done
+
+    source build/envsetup.sh
+    breakfast ${deviceCode}
   '';
 
   shellHook = ''
     if [ -r default.xml ]; then
-      eval $postUnpack
+      eval "$postUnpack"
     fi
   '';
 }
